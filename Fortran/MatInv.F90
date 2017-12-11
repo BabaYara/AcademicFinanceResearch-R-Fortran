892a2eb497df33ba945207b8fb883727f1b86edc
Fahiz Baba Yara <fahiz62@live.com>
2017-12-11 21:54:45 +0000

    Update MatInv.F90

--- Fortran/MatInv.F90
+++ Fortran/MatInv.F90
@@ -2,7 +2,7 @@
 subroutine MatInv( A, Ainv)
    implicit none
    real*8,        intent(in)   ::  A(:,:)
-   real*8         intent(out)  ::  Ainv(:,:)
+   real*8,        intent(out)  ::  Ainv(:,:)
    real*8, allocatable         ::  work(:)
    integer                     :: n, info
    integer, allocatable        :: ipiv(:)
@@ -14,5 +14,5 @@ subroutine MatInv( A, Ainv)
    call dgetrf(n,n,Ainv,n,ipiv,info)
    call dgetri(n,Ainv,n,ipiv,work,n,info)
    
-   deallocate(ipiv,work)
+   deallocate( ipiv, work)
 end subroutine MatInv
