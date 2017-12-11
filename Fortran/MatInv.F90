0e23ea21e286abd7b11dd9450c5aa41d23441e0c
Fahiz Baba Yara <fahiz62@live.com>
2017-12-11 23:10:19 +0000

    Update MatInv.F90

--- Fortran/MatInv.F90
+++ Fortran/MatInv.F90
@@ -1,18 +1,19 @@
-! Computes inverse of a matrix
-subroutine MatInv( A, Ainv)
-   implicit none
-   real*8,        intent(in)   ::  A(:,:)
-   real*8,        intent(out)  ::  Ainv(:,:)
-   real*8, allocatable         ::  work(:)
-   integer                     :: n, info
-   integer, allocatable        :: ipiv(:)
+  subroutine inv(A, Ainv)
+    implicit none
+    real*8,intent(in) :: A(:,:)
+    real*8            :: Ainv(size(A,1),size(A,2))
+    real*8            :: work(size(A,1))            ! work array for LAPACK
+    integer           :: n,info,ipiv(size(A,1))     ! pivot indices
 
-   Ainv  =  A
-   n     =  size( Ainv, 1)
-   allocate(ipiv(n),work(n))
-   
-   call dgetrf(n,n,Ainv,n,ipiv,info)
-   call dgetri(n,Ainv,n,ipiv,work,n,info)
-   
-   deallocate( ipiv, work)
-end subroutine MatInv
+    ! Store A in Ainv to prevent it from being overwritten by LAPACK
+    Ainv = A
+    n = size(A,1)
+    ! SGETRF computes an LU factorization of a general M-by-N matrix A
+    ! using partial pivoting with row interchanges.
+    call DGETRF(n,n,Ainv,n,ipiv,info)
+    if (info.ne.0) stop 'Matrix is numerically singular!'
+    ! SGETRI computes the inverse of a matrix using the LU factorization
+    ! computed by SGETRF.
+    call DGETRI(n,Ainv,n,ipiv,work,n,info)
+    if (info.ne.0) stop 'Matrix inversion failed!'
+  end subroutine inv
